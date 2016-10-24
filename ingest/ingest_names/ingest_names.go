6d3f63a47a6441b37d3652e67e6d76abb3f7acd5
Jeff Buchbinder <jeff@ourexchange.net>
2016-10-24 12:26:03 -0400

    Initial commit -- name + zip.

---
+++ ingest/ingest_names/ingest_names.go
@@ -0,0 +1,91 @@
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"errors"
+	"flag"
+	"fmt"
+	"io"
+	"os"
+	"strings"
+)
+
+var (
+	Path    = flag.String("path", "../../source", "Path to data")
+	Package = flag.String("package", "main", "Go package name")
+)
+
+func main() {
+	flag.Parse()
+
+	fmt.Printf("package %s\n\n", *Package)
+	fmt.Printf("var (\n")
+
+	{
+		f, err := fileToArray("dist.male.first")
+		if err != nil {
+			panic(err)
+		}
+		fmt.Printf(arrayToString("NameGivenMale", f))
+	}
+	{
+		f, err := fileToArray("dist.female.first")
+		if err != nil {
+			panic(err)
+		}
+		fmt.Printf(arrayToString("NameGivenFemale", f))
+	}
+	{
+		f, err := fileToArray("dist.all.last")
+		if err != nil {
+			panic(err)
+		}
+		fmt.Printf(arrayToString("NameSurname", f))
+	}
+
+	fmt.Printf(")\n\n")
+}
+
+func fileToArray(fn string) ([]string, error) {
+	out := make([]string, 0)
+
+	if !fileExists(*Path + string(os.PathSeparator) + fn) {
+		return out, errors.New("Data file not found")
+	}
+	f, err := os.Open(*Path + string(os.PathSeparator) + fn)
+	if err != nil {
+		return out, err
+	}
+	defer f.Close()
+	r := bufio.NewReader(f)
+	for {
+		path, err := r.ReadString(10) // 0x0A separator = newline
+		if err == io.EOF {
+			return out, nil
+			break
+		} else if err != nil {
+			return out, err
+		}
+		if len(path) < 10 {
+			return out, nil
+			break
+		}
+		// Only ingest the first word
+		out = append(out, path[0:strings.Index(path, " ")])
+	}
+	return out, nil
+}
+
+func arrayToString(name string, items []string) string {
+	var buf bytes.Buffer
+	buf.WriteString(fmt.Sprintf("\t%s = []string { ", name))
+	for i := range items {
+		buf.WriteString(fmt.Sprintf(`"%s"`, items[i]))
+		if i+1 < len(items) {
+			buf.WriteString(", ")
+		}
+	}
+	buf.WriteString(" }\n")
+	return buf.String()
+}
---
+++ ingest/ingest_zip/ingest_zip.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"encoding/csv"
+	"flag"
+	"fmt"
+	"io"
+	"os"
+)
+
+var (
+	Source  = flag.String("source", "../../source/free-zipcode-database-Primary.csv", "Path to data file")
+	Package = flag.String("package", "main", "Go package name")
+)
+
+func main() {
+	flag.Parse()
+
+	fmt.Printf("package %s\n\n", *Package)
+	fmt.Printf(`
+typedef Location struct {
+	Zipcode string
+	City string
+	State string
+	LocationType string
+	Lat double
+	Long double
+	Location string
+	Decommisioned bool
+}
+`)
+	fmt.Printf("var (\n")
+
+	// Open and parse
+	if !fileExists(*Source) {
+		panic("Unable to open source file")
+	}
+	f, err := os.Open(*Source)
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+
+	r := csv.NewReader(f)
+
+	fmt.Printf("\tLocations = []Location{ ")
+	i := 0
+	for {
+		record, err := r.Read()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			panic(err)
+		}
+
+		if i == 0 {
+			i++
+			continue
+		} // skip header
+
+		// Skip missing lat/lng
+		if record[5] == "" || record[6] == "" {
+			i++
+			continue
+		}
+
+		fmt.Printf("\t\t"+`Location{"%s", "%s", "%s", "%s", %s, %s, "%s", %s}, `+"\n", record[0], record[1], record[3], record[4], record[5], record[6], record[7], record[8])
+		i++
+	}
+	fmt.Printf("\t}\n")
+
+	fmt.Printf(")\n\n")
+}
